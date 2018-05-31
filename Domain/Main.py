70b3049efb0df9c098a438539c187fa1822bb333
Vlad <vlad.gamedev@gmail.com>
2018-05-31 14:13:03 +0200

    done

--- Domain/Main.py
+++ Domain/Main.py
@@ -9,11 +9,13 @@ centroid_2 = Centroid(np.array([5.936, 2.77, 4.26, 1.326], dtype=float), "Iris-v
 centroid_3 = Centroid(np.array([6.588, 2.974, 5.552, 2.026], dtype=float), "Iris-virginica")
 centroids = (centroid_1, centroid_2, centroid_3)
 
+epsilon = 1
+
 with open("../Resources/iris.txt", "r") as file:
     lines = csv.reader(file, delimiter=",")
     for line in lines:
         cur_iris = Iris(np.array((line[0], line[1], line[2], line[3]), dtype=float), line[4])
-        cur_iris.set_distances(centroids[0].coordinates, centroids[1].coordinates, centroids[2].coordinates)
+        cur_iris.set_distances(centroid_1.coordinates, centroid_2.coordinates, centroid_3.coordinates)
         min_distance = Utility.calc_distance(centroids[0].coordinates, cur_iris.coordinates)
         target_centroid = centroids[0]
         for centroid in centroids:
@@ -21,7 +23,7 @@ with open("../Resources/iris.txt", "r") as file:
             if min_distance > cur_distance:
                 min_distance = cur_distance
                 target_centroid = centroid
-        target_centroid.approximate(cur_iris.distances, 1)
+        target_centroid.approximate(cur_iris.distances, epsilon)
 
 for centroid in centroids:
     print(len(centroid.high_approximation))
