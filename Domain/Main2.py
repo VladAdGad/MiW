247e28a6daa31843e8c516da2876b50347816880
Vlad <vlad.gamedev@gmail.com>
2018-06-06 16:00:07 +0200

    updated main2

--- Domain/Main2.py
+++ Domain/Main2.py
@@ -1,9 +1,28 @@
-import csv, sys
+import csv
 import math
-import enum
-import random
 
 from statistics import mean
+
+
+class Iris:
+    all = set()
+
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.nodes = set()
+        Iris.all.add(self)
+
+    def distance(self, n):
+        return round(math.pow((self.a - n.a), 2) + \
+                     math.pow((self.b - n.b), 2) + \
+                     math.pow((self.c - n.c), 2) + \
+                     math.pow((self.d - n.d), 2))
+
+    def __str__(self):
+        return "[{},{},{},{}]".format(self.a, self.b, self.c, self.d)
 
 
 class Cluster:
@@ -14,42 +33,21 @@ class Cluster:
         self.b = b
         self.c = c
         self.d = d
-        self.nodes = set()
-        Cluster.all.add(self)
-
-    def calc_dist(self, n):
-        return round(math.pow((self.a - n.a), 2) + \
-                     math.pow((self.b - n.b), 2) + \
-                     math.pow((self.c - n.c), 2) + \
-                     math.pow((self.d - n.d), 2))
-
-    def __str__(self):
-        return "[{},{},{},{}]".format(self.a, self.b, self.c, self.d)
-
-
-class Centroid:
-    all_clusters = set()
-
-    def __init__(self, a, b, c, d):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
-        self.clusters = list()
-        self.all_clusters.add(self)
+        self.nodes = list()
+        self.all.add(self)
 
     def add(self, node):
-        self.clusters.append(node)
+        self.nodes.append(node)
 
     def recalc_centroid(self):
-        self.a = round(mean([n.a for n in self.clusters]), 2)
-        self.b = round(mean([n.b for n in self.clusters]), 2)
-        self.c = round(mean([n.c for n in self.clusters]), 2)
-        self.d = round(mean([n.d for n in self.clusters]), 2)
+        self.a = round(mean([n.a for n in self.nodes]), 2)
+        self.b = round(mean([n.b for n in self.nodes]), 2)
+        self.c = round(mean([n.c for n in self.nodes]), 2)
+        self.d = round(mean([n.d for n in self.nodes]), 2)
 
     def calc_Q(self):
-        isolation = sum(node.distance(c) for c in self.all_clusters for node in self.clusters if c != self)
-        s = sum(node.distance(self) for node in self.clusters)
+        isolation = sum(node.distance(c) for c in self.all for node in self.nodes if c != self)
+        s = sum(node.distance(self) for node in self.nodes)
         return round(isolation / s, 2)
 
     def __str__(self):
@@ -66,22 +64,22 @@ def readFile(file):
             b = float(row[1])
             c = float(row[2])
             d = float(row[3])
-            node = Cluster(a, b, c, d)
+            node = Iris(a, b, c, d)
             yield node
 
 
 if __name__ == '__main__':
-    nodes = list(readFile("iris.txt"))
-    c1 = Centroid(5.01, 3.42, 1.46, 0.24)
-    c2 = Centroid(6.59, 2.97, 5.55, 2.03)
-    c3 = Centroid(5.94, 2.77, 4.26, 1.33)
+    nodes = list(readFile("../Resources/iris.txt"))
+    c1 = Cluster(5.01, 3.42, 1.46, 0.24)
+    c2 = Cluster(6.59, 2.97, 5.55, 2.03)
+    c3 = Cluster(5.94, 2.77, 4.26, 1.33)
     cluster_q = dict()
     # assignment
     for node in nodes:
         distances = {}
-        distances[node.calc_dist(c1)] = c1
-        distances[node.calc_dist(c2)] = c2
-        distances[node.calc_dist(c3)] = c3
+        distances[node.distance(c1)] = c1
+        distances[node.distance(c2)] = c2
+        distances[node.distance(c3)] = c3
         distances[min(distances.keys())].add(node)
     cluster_q[c1] = c1.calc_Q()
     cluster_q[c2] = c2.calc_Q()
